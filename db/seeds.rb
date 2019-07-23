9bb58e2be7f998e6cf1970126462d8f16c510525
Ryota Inakazu <mail.to.inakazu@gmail.com>
2019-07-23 15:54:10 +0900

    chore: register initial users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if AdminUser.find_by(email: 'admin@example.com').blank?
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+end
+
+if User.all.blank?
+  10.times do |i|
+    User.create!(name: "user%02d" % i, age: 10 + i)
+  end
+end
